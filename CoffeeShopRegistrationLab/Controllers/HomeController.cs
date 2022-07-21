19bbd29806cef104f4e938e45f9e6e792a9f9a16
Connor <Woodconnor13@gmail.com>
2022-07-21 15:27:08 -0400

    finished registration lab

---
+++ CoffeeShopRegistrationLab/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using CoffeeShopRegistrationLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopRegistrationLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult CustomerForm()
+        {
+            return View();
+        }
+
+        public IActionResult CustomerResult(Customer c)
+        {
+            return View(c);
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoffeeShopRegistrationLab/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeShopRegistrationLab.Models
+{
+    public class Customer
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
